cff29e9ef4fb0a4a5e8af4c2ecf82517de345002
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-25 14:23:17 +0100

    Cooking...

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}

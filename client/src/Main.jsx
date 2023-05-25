42c96ab4a53ff58f3ac2529aa82a97376d4e7855
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-25 14:31:30 +0100

    Cooking...

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { Background } from "./Theme/Index"
+import { ThemeProvider } from './Contexts/ThemeContext'
 import App from './App.jsx'
+import "./Styles/Index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Background>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Background>
   </React.StrictMode>,
 )

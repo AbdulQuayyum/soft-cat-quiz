f6605f046439c0166ed46e52ea5b91285ccfeca4
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-27 00:35:18 +0100

    Cooking...

--- client/src/Layout/Main.Layout.jsx
+++ client/src/Layout/Main.Layout.jsx
@@ -5,7 +5,7 @@ export default function MainLayout(props) {
     const { children } = props
 
     return (
-        <div>
+        <div className='relative z-10 min-h-screen flex justify-between items-center flex-col max-w-7xl mx-auto sm:px-16 px-6'>
             <Navbar />
             {children}
             <Footer />
--- client/src/Routes/Main.Routes.jsx
+++ client/src/Routes/Main.Routes.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 
 import { Home, Quiz, Result } from "../Views/Index"
+import MainLayout from '../Layout/Main.Layout';
 
 const MainRoutes = () => {
     return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/Quiz' element={<Quiz />} />
-            <Route path='/Result' element={<Result />} />
-        </Routes>
+        <MainLayout>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/Quiz' element={<Quiz />} />
+                <Route path='/Result' element={<Result />} />
+            </Routes>
+        </MainLayout>
     )
 }
 

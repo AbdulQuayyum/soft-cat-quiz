39b7a1d158316118f668a06a22b92958c29a6425
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-27 00:57:28 +0100

    Cooking...

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+    const inputRef = useRef(null)
+
     return (
-        <div>Home</div>
+        <div>
+            <div className="">
+                <div className='w-full flex justify-center'>
+                    <span className='text-3xl py-6'>Cat Quiz</span>
+                </div>
+                <ol>
+                    <li>You will be asked 10 questions one after another.</li>
+                    <li>10 points is awarded for the correct answer.</li>
+                    <li>Each question has three options. You can choose only one options.</li>
+                    <li>You can review and change answers before the quiz finish.</li>
+                    <li>The result will be declared at the end of the quiz.</li>
+                </ol>
+
+                <div className='my-6'>
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        // value={firstAmount}
+                        // onChange={e => setFirstAmount(e.target.value)}
+                        placeholder="Username"
+                        className="p-2 text-lg w-full rounded-xl transition-all duration-500 border-2 border-gray-200 outline-none dark:bg-transparent dark:border-2 dark:rounded-lg dark:border-white"
+                    />
+                </div>
+                <div className='flex justify-end'>
+                    <Link
+                        to={"Quiz"}
+                        className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white'>
+                        Start Quiz
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
 

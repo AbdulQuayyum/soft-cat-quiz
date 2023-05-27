a2facb06e65a6c2b18c03d6bd75eb65e17862cc5
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-27 01:14:37 +0100

    Cooking...

--- client/src/Views/Quiz.jsx
+++ client/src/Views/Quiz.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Quiz = () => {
     return (
-        <div></div>
+        <div className='container'>
+            <div className='w-full flex justify-center'>
+                <span className='text-3xl py-6'>Cat Quiz</span>
+            </div>
+            <div className='flex justify-between'>
+                <button className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white' >Prev</button>
+                <button className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white' >Next</button>
+            </div>
+
+        </div>
     )
 }
 

ae88fe706448f0ec5bb9c7c62a08e359863ee779
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-27 02:51:14 +0100

    Cooking...

--- client/src/Components/Index.jsx
+++ client/src/Components/Index.jsx
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Questions from "./Questions";
+import ResultTable from "./ResultTable";
 
-export { Footer, Navbar, Questions }
+export { Footer, Navbar, Questions, ResultTable }
---
+++ client/src/Components/ResultTable.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ResultTable() {
+    return (
+        <div>ResultTable</div>
+    )
+}

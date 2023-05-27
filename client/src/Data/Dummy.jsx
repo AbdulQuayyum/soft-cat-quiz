01399677ee3cf73730ab75dad7f1a8a7ee0a30ad
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-27 01:55:05 +0100

    Cooking...

---
+++ client/src/Data/Dummy.jsx
@@ -0,0 +1,94 @@
+export default [
+    {
+        id: 1,
+        question: "A cat is pregnant for how long?",
+        options: [
+            '10 and 20 days',
+            '100 and 120 days',
+            '60 and 70 days',
+        ]
+    },
+    {
+        id: 2,
+        question: "What is a baby cat called?",
+        options: [
+            'A Kid',
+            'A Kitten',
+            'A Puppy',
+        ]
+    },
+    {
+        id: 3,
+        question: "What is a group of cats called?",
+        options: [
+            'A Gang',
+            'A Clowder',
+            'A Bunch',
+        ]
+    },
+    {
+        id: 4,
+        question: "What is the fluttering sound made by a cat called?",
+        options: [
+            'A Purr',
+            'A Bark',
+            'A Bleat',
+        ]
+    },
+    {
+        id: 5,
+        question: "How many teeth does an adult cat have?",
+        options: [
+            '30',
+            '24',
+            '20',
+        ]
+    },
+    {
+        id: 6,
+        question: "What is a male cat called?",
+        options: [
+            'A Garfield',
+            'A Tom',
+            'A King',
+        ]
+    },
+    {
+        id: 7,
+        question: "What is a litter of kittens called?",
+        options: [
+            'A Kindle',
+            'A Whelp',
+            'A Farrow',
+        ]
+    },
+    {
+        id: 8,
+        question: "What is a female cat called?",
+        options: [
+            'A Princess',
+            'An Empress',
+            'A Queen',
+        ]
+    },
+    {
+        id: 9,
+        question: "When your cat rubs you, what is he showing?",
+        options: [
+            'Friendship',
+            'Ownership',
+            'Aggression',
+        ]
+    },
+    {
+        id: 10,
+        question: "In what year was the first cat sent to space?",
+        options: [
+            '2004',
+            '1999',
+            '1963',
+        ]
+    },
+]
+
+export const answers = [2, 1, 1, 0, 0, 1, 0, 2, 0, 2]
--- client/src/Views/Quiz.jsx
+++ client/src/Views/Quiz.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
+import { Questions } from "../Components/Index"
 
 const Quiz = () => {
+
+    const onNext = () => { }
+    const onPrev = () => { }
+
     return (
         <div className='container'>
             <div className='w-full flex justify-center'>
                 <span className='text-3xl py-6'>Cat Quiz</span>
             </div>
+            <div className='flex justify-center'>
+                <Questions />
+            </div>
             <div className='flex justify-between'>
-                <button className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white' >Prev</button>
-                <button className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white' >Next</button>
+                <button
+                    onClick={onPrev}
+                    className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white'>
+                    Prev
+                </button>
+                <button
+                    onClick={onNext}
+                    className='rounded-full border border-black bg-black py-3 px-8 text-sm text-white transition-all hover:bg-white hover:text-black dark:bg-white dark:text-black dark:hover:bg-black dark:hover:text-white'>
+                    Next
+                </button>
             </div>
 
         </div>

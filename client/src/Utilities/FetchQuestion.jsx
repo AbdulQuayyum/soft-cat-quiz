c68a1ae60269c3c94ed714df47dfed8ed126bacd
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-28 12:02:36 +0100

    Cooking...

--- client/src/Utilities/FetchQuestion.jsx
+++ client/src/Utilities/FetchQuestion.jsx
@@ -1,7 +1,43 @@
 import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux"
 
-export const UseFetchQuestion = () => { }
+import * as Action from "../Redux/QuestionReducer"
+import Dummy from "../Data/Dummy"
+
+export const UseFetchQuestion = () => {
+    const dispatch = useDispatch()
+    const [getData, setGetData] = useState({ IsLoading: false, APIData: [], ServerError: null })
+
+    useEffect(() => {
+        setGetData(prev => ({ ...prev, IsLoading: true }))
+
+            (async () => {
+                try {
+
+                    let Questions = await Dummy
+
+                    if (Questions.length > 0) {
+                        setGetData(prev => ({ ...prev, IsLoading: false }));
+                        setGetData(prev => ({ ...prev, APIData: Questions }));
+
+                        /** dispatch an action */
+                        dispatch(Action.StartExamAction({ Question: Questions, Answers }))
+
+                    } else {
+                        throw new Error("No Question Avalibale");
+                    }
+
+                } catch (error) {
+                    setGetData(prev => ({ ...prev, IsLoading: false }));
+                    setGetData(prev => ({ ...prev, ServerError: error }));
+                }
+            })()
+
+    }, [dispatch])
+
+    return [getData, setGetData]
+
+}
 
 export const MoveNextQuestion = () => async (dispatch) => { }
 

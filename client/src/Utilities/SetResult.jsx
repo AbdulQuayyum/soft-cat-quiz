55a9eabcc653903adbd7a28267a6f88b37b3edc6
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-01 02:02:42 +0100

    Cooking...

--- client/src/Utilities/SetResult.jsx
+++ client/src/Utilities/SetResult.jsx
@@ -1,5 +1,15 @@
-export const PushAnswer = (Result) => async (dispatch) => { }
+import * as Action from "../Redux/ResultReducer"
 
-export const UpdateResult = (Index) => async (dispatch) => { }
+export const PushAnswer = (Result) => async (dispatch) => {
+    try {
+        await dispatch(Action.PushResultAction(Result))
+    } catch (error) { console.error(error) }
+}
+
+export const UpdateResult = (Index) => async (dispatch) => {
+    try {
+        dispatch(Action.UpdateResultAction(Index))
+    } catch (error) { console.error(error) }
+}
 
 export const UsePublishResult = (ResultData) => { }

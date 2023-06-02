e9e91e638f93ca89057e27e41d241a2ee0f022a1
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-02 21:29:17 +0100

    Cooking...

---
+++ Server/Controllers/Controller.js
@@ -0,0 +1,21 @@
+import Questions from "../Models/QuestionSchema"
+import Result from "../Models/ResultSchema"
+import NewQuestions, { Answers } from "../Database/Data"
+
+/** get all questions */
+export async function GetQuestions(req, res) { }
+
+/** insert all questinos */
+export async function InsertQuestions(req, res) { }
+
+/** Delete all Questions */
+export async function DropQuestions(req, res) { }
+
+/** get all result */
+export async function GetResult(req, res) { }
+
+/** post all result */
+export async function StoreResult(req, res) { }
+
+/** delete all result */
+export async function DropResult(req, res) { }

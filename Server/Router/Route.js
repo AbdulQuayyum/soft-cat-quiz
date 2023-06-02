0e34a05501b26b2bc840e60e9289832881ea2bbf
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-02 21:31:33 +0100

    Cooking...

--- Server/Router/Route.js
+++ Server/Router/Route.js
@@ -1,4 +1,18 @@
 import { Router } from "express"
 const router = Router()
 
+import * as Controller from "../Controllers/Controller"
+
+/** Questions Routes API */
+
+router.route('/Questions')
+    .get(Controller.GetQuestions) /** GET Request */
+    .post(Controller.InsertQuestions) /** POST Request */
+    .delete(Controller.DropQuestions) /** DELETE Request */
+
+router.route('/Result')
+    .get(Controller.GetResult)
+    .post(Controller.StoreResult)
+    .delete(Controller.DropResult)
+
 export default router

0d55aee45fc4ee0c43fe300a1f0dbc6eb15356b7
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-06 17:49:32 +0100

    Cooking...

--- Server/Database/Connect.js
+++ Server/Database/Connect.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export default async function Connect() {
+    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGO_URI)
     console.log("Connected")
 }
